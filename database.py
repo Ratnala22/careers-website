14d8f043528cc9588d78f5d39d8588b27321a0fc
Ratnala22 <68419466+Ratnala22@users.noreply.github.com>
2024-01-14 07:23:31 +0000

    dynamic data loaded

--- database.py
+++ database.py
@@ -10,3 +10,11 @@ def load_jobs_from_db():
       list.append(row._asdict())
     return list
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result=conn.execute(text(f"select * from jobs where id = {id}"))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
