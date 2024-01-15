3e6393bb90edef166f486f39070e732b8a6bc8c4
Ratnala22 <68419466+Ratnala22@users.noreply.github.com>
2024-01-15 09:00:18 +0000

    adding data to the database on cloud

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine,text,Table, Column, Integer, String, insert
+
 db_connection_string="mysql+pymysql://sql5676383:2xDB9Xw9lm@sql5.freesqldatabase.com/sql5676383?charset=utf8mb4"
 engine = create_engine(db_connection_string)
 
@@ -19,3 +20,26 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+      
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+      query = text(
+          "INSERT INTO application (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+      )
+
+      params = {
+          'job_id': job_id,
+          'full_name': data['full_name'],
+          'email': data['email'],
+          'linkedin_url': data['linkedin_url'],
+          'education': data['education'],
+          'work_experience': data['work_experience'],
+          'resume_url': data['resume_url']
+      }
+
+      print(f"Executing Query: {query}, Params: {params}")
+      result = conn.execute(query, params)
+
+      print(f"Rows Affected: {result.rowcount}")
+
+
